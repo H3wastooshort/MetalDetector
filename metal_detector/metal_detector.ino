b37158438b6d3ca28c2af99e21dd4c96e5153a83
HACKER3000 <hacker3000@posteo.org>
2023-02-12 01:26:56 +0100

    fixed freq display and bar. added graph
    some things still kinda fucky

--- metal_detector/metal_detector.ino
+++ metal_detector/metal_detector.ino
@@ -6,11 +6,11 @@
 #define SHORT_CLICK_TIME 50  //minimum time for a short click to register
 #define LONG_CLICK_TIME 500
 
-uint32_t pulse_array[10] = { 0 };  //ring buffer of latest pulsecounts
+uint32_t pulse_array[5] = { 0 };  //ring buffer of latest pulsecounts
 constexpr uint8_t pulse_array_length = sizeof(pulse_array) / sizeof(pulse_array[0]);
 uint8_t pulse_array_next = 0;
 uint32_t pulses_this_int = 0;  //how many pulses since the last timer interrupt
-#define TIMER_FREQ 2           //how often the timer int fires, in Hertz
+#define TIMER_FREQ 4           //how often the timer int fires, in Hertz
 constexpr uint8_t TIMER_CMP =  //timer compare value. RESULT MUST BE SMALLER THAN 255, otherwise increase prescaler in setup()
   (F_CPU /*cpu freq*/ / (16348L /*prescaler*/ * (uint16_t)TIMER_FREQ /*timer freqency*/)) - 1;
 
@@ -26,10 +26,9 @@ uint8_t beep_flag = 2;  // 0 or 1 mean beep, 2 means off, 3 means passthru
 ISR(PCINT0_vect) {
   pulses_this_int++;
   //if (beep_flag == 3) PORTB &= (0xFF & (uint8_t(PINB && 0b00001000) << 4 /*out pin*/)); //set bit for beeper to bit of freq input
-  if (beep_flag == 3) digitalWrite(4,digitalRead(3));
+  if (beep_flag == 3) digitalWrite(4, digitalRead(3));
 }
 
-bool timer_test = false;
 ISR(TIMER1_COMPA_vect) {
   pulse_array[pulse_array_next] = pulses_this_int;  //save pulse count
   pulses_this_int = 0;                              //reset pulsecount
@@ -41,9 +40,6 @@ ISR(TIMER1_COMPA_vect) {
     beep_flag ^= 1;  //toggle LSB by XOR with 1
     digitalWrite(4, beep_flag);
   }
-
-  timer_test = !timer_test;
-  digitalWrite(5, timer_test);
 }
 
 float get_pulses() {
@@ -109,7 +105,6 @@ void setup() {
   pinMode(1, INPUT_PULLUP);  //button
   pinMode(3, INPUT_PULLUP);  //freq
   pinMode(4, OUTPUT);        //speaker
-  pinMode(5, OUTPUT);        //testing
 
   lcd.init();
   Wire.setClock(400000);
@@ -137,43 +132,66 @@ void setup() {
 }
 
 void draw_display() {
-  //lcd.clear();
-  lcd.home();
   uint32_t pulses = get_pulses();
-  //set custom chars 6 and 7 as diagram (TODO)
-  char row1[17];
-  //snprintf(row1, 17, "Freq: %8.3fHz", freq);
-  snprintf(row1, 17, "\x06\x07 Freq: %5luHz", (pulses / 2) * TIMER_FREQ);
-  lcd.print(row1);
 
-  lcd.setCursor(0, 1);
-  uint8_t bars = map(pulses, cal_data.pulses_air, cal_data.pulses_iron * 2, 16, 0);
-  for (uint8_t i = 16; i > 0; i--) {
-    if (i >= bars) lcd.write(255);
-    else lcd.write(' ');
-  }
+  if (pulses > 0) {
+    //set custom char 7 as diagram
+    byte graph_char[8] = { 0 };
+    for (uint8_t col = 0; col < 5; col++)
+      graph_char[max(min(map(pulse_array[col], cal_data.pulses_iron / 2, cal_data.pulses_air * 2, 0, 7), 7), 0)] =
+        (1 << (5 - col));
+    lcd.createChar(0x07, graph_char);
 
-  if (beep_flag != 3) {  //if passthru disabled
-    /*if (pulses < cal_data.pulses_iron) tone(4, 500);
-  else noTone(4);*/
-    if (pulses < cal_data.pulses_iron) beep_flag = 1;  //turn beep on
-    else {
-      beep_flag = 2;  //turn beep off
-      digitalWrite(4, LOW);
+    //lcd.clear();
+    lcd.home();
+
+    char line1[17];
+    //snprintf(row1, 17, "Freq: %8.3fHz", freq);
+    snprintf(line1, 17, "Freq: \x07 %6luHz", (pulses / 2) * TIMER_FREQ);
+    lcd.print(line1);
+
+    lcd.setCursor(0, 1);
+    uint8_t bars = max(min(map(pulses, cal_data.pulses_air, cal_data.pulses_iron - (cal_data.pulses_air - cal_data.pulses_iron), 0, 16), 16), 0);
+    for (uint8_t i = 1; i <= 16; i++) {
+      if (i > bars) lcd.write(i == 8 ? '|' : ' ');
+      else lcd.write(255);
     }
+
+    if (beep_flag != 3) {  //if passthru disabled
+      /*if (pulses < cal_data.pulses_iron) tone(4, 500);
+  else noTone(4);*/
+      if (pulses < cal_data.pulses_iron) beep_flag = 1;  //turn beep on
+      else {
+        beep_flag = 2;  //turn beep off
+        digitalWrite(4, LOW);
+      }
+    }
+  } else {
+    beep_flag = 2;  //turn beep off
+    digitalWrite(4, LOW);
+
+    lcd.clear();
+    lcd.home();
+    lcd.print(F("Osciltr. stopped"));
+    lcd.setCursor(0, 1);
+    lcd.print(F("   CHECK COIL!  "));
   }
 }
 
 void loop() {
   static uint32_t last_disp_update = 0;
-  if (millis() - last_disp_update > 2000) {
+  if (millis() - last_disp_update > 1000) {
     last_disp_update = millis();
     draw_display();
   }
 
+  static bool last_bl = false;
   switch (get_btn()) {
     case 2:
-      beep_flag = beep_flag == 3 ? 2 : 3;  //toggle between passthru and off
+      //beep_flag = beep_flag == 3 ? 2 : 3;  //toggle between passthru and off
+
+      last_bl ? lcd.backlight() : lcd.noBacklight();
+      last_bl = !last_bl;
       break;
     case 3:
       do_cal();
